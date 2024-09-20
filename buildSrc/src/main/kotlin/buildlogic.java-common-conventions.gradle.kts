4613bdd34d9e2d773e7ad22470e9b9b2d11c37ff
Johannes Link <business@johanneslink.net>
2024-09-20 15:31:58 +0200

    Added some Java compile options

--- buildSrc/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
@@ -17,13 +17,23 @@ dependencies {
 val javaVersion: String = System.getProperty("matrix.version") ?: "21"
 val javaVendor: String = System.getProperty("matrix.vendor") ?: "adoptium"
 
-println("Using Java version: $javaVersion - $javaVendor")
+// println("Using Java version: $javaVersion - $javaVendor")
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(javaVersion)
         vendor = JvmVendorSpec.matching(javaVendor)
     }
+
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.compileTestJava {
+    // Enable source parameter names through reflection
+    options.compilerArgs.add("-parameters")
 }
 
 tasks.named<Test>("test") {
